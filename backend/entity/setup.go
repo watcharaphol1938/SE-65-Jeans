ab98e9a612921ab0d692cb8c28b20743ce08a41f
watcharaphol chareerat <jeanswc1938@gmail.com>
2023-01-14 00:18:22 +0700

    controller Entity Nationality, main, setup - close #17

--- backend/entity/setup.go
+++ backend/entity/setup.go
@@ -18,6 +18,6 @@ func SetupDatabase() {
   }
 
   // Migrate the schema
-  database.AutoMigrate(&PatientRegister{}, &Employee{}, &Gender{}, &Prefix{})
+  database.AutoMigrate(&PatientRegister{}, &Employee{}, &Gender{}, &Prefix{}, &Nationality{})
   db = database
 }
