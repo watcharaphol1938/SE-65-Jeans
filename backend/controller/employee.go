09c4e928f14298a6b9c7d69fe6b8f0ed6df712e4
watcharaphol chareerat <jeanswc1938@gmail.com>
2023-01-13 18:25:22 +0700

    controller Entity Employee - close #14

---
+++ backend/controller/employee.go
@@ -0,0 +1,144 @@
+package controller
+
+import (
+	"github.com/watcharaphol1938/SE-65-Jeans/entity"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+/*	POST /users
+	func CreateUser(c *gin.Context) {
+		var user entity.User
+		if err := c.ShouldBindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		if err := entity.DB().Create(&user).Error; err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"data": user})
+	}
+*/
+
+// POST /employees
+func CreateEmployee(c *gin.Context) {
+	var employee entity.Employee
+	if err := c.ShouldBindJSON(&employee); err != nil {
+		   c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		   return
+	}
+	if err := entity.DB().Create(&employee).Error; err != nil {
+		   c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		   return
+	}
+	c.JSON(http.StatusOK, gin.H{"data": employee})
+}
+
+// *******************************************************************************************************
+
+// GET /user/:id
+// func GetUser(c *gin.Context) {
+// 	var user entity.User
+// 	id := c.Param("id")
+// 	if err := entity.DB().Raw("SELECT * FROM users WHERE id = ?", id).Scan(&user).Error; err != nil {
+// 		   c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+// 		   return
+// 	}
+// 	c.JSON(http.StatusOK, gin.H{"data": user})
+// }
+
+// GET /employee/:id
+func GetEmployee(c *gin.Context) {
+	var employee entity.Employee
+	id := c.Param("id")
+	if err := entity.DB().Raw("SELECT * FROM employees WHERE id = ?", id).Scan(&employee).Error; err != nil {
+		   c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		   return
+	}
+	c.JSON(http.StatusOK, gin.H{"data": employee})
+}
+
+// *******************************************************************************************************
+
+
+// GET /users
+// func ListUsers(c *gin.Context) {
+// 	var users []entity.User
+// 	if err := entity.DB().Raw("SELECT * FROM users").Scan(&users).Error; err != nil {
+// 		   c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+// 		   return
+// 	}
+// 	c.JSON(http.StatusOK, gin.H{"data": users})
+// }
+
+// GET /employees
+func ListEmployees(c *gin.Context) {
+	var employees []entity.Employee
+	if err := entity.DB().Raw("SELECT * FROM employees").Scan(&employees).Error; err != nil {
+		   c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		   return
+	}
+	c.JSON(http.StatusOK, gin.H{"data": employees})
+}
+
+// *******************************************************************************************************
+
+
+// DELETE /users/:id
+// func DeleteUser(c *gin.Context) {
+// 	id := c.Param("id")
+// 	if tx := entity.DB().Exec("DELETE FROM users WHERE id = ?", id); tx.RowsAffected == 0 {
+// 		   c.JSON(http.StatusBadRequest, gin.H{"error": "user not found"})
+// 		   return
+// 	}
+// 	c.JSON(http.StatusOK, gin.H{"data": id})
+// }
+
+// DELETE /employees/:id
+func DeleteEmployee(c *gin.Context) {
+	id := c.Param("id")
+	if tx := entity.DB().Exec("DELETE FROM employees WHERE id = ?", id); tx.RowsAffected == 0 {
+		   c.JSON(http.StatusBadRequest, gin.H{"error": "employee not found"})
+		   return
+	}
+	c.JSON(http.StatusOK, gin.H{"data": id})
+}
+
+// *******************************************************************************************************
+
+// PATCH /users
+// func UpdateUser(c *gin.Context) {
+// 	var user entity.User
+// 	if err := c.ShouldBindJSON(&user); err != nil {
+// 		   c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+// 		   return
+// 	}
+// 	if tx := entity.DB().Where("id = ?", user.ID).First(&user); tx.RowsAffected == 0 {
+// 		   c.JSON(http.StatusBadRequest, gin.H{"error": "user not found"})
+// 		   return
+// 	}
+// 	if err := entity.DB().Save(&user).Error; err != nil {
+// 		   c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+// 		   return
+// 	}
+// 	c.JSON(http.StatusOK, gin.H{"data": user})
+// }
+
+// PATCH /employees
+func UpdateEmployee(c *gin.Context) {
+	var employee entity.Employee
+	if err := c.ShouldBindJSON(&employee); err != nil {
+		   c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		   return
+	}
+	if tx := entity.DB().Where("id = ?", employee.ID).First(&employee); tx.RowsAffected == 0 {
+		   c.JSON(http.StatusBadRequest, gin.H{"error": "employee not found"})
+		   return
+	}
+	if err := entity.DB().Save(&employee).Error; err != nil {
+		   c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		   return
+	}
+	c.JSON(http.StatusOK, gin.H{"data": employee})
+}
